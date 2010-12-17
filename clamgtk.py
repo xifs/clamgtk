525dc98c0b9ad5e8d6b5eec10b39b89783a327cf
vechilee <lizaifang@gmail.com>
2010-12-17 11:02:10 +0800

    change filechoose method, remove filechoose from glade file

--- clamgtk.py
+++ clamgtk.py
@@ -1,3 +1,4 @@
+#encoding:utf8
 import os,gtk,pyclamd
 
 class ClamGtk():
@@ -25,23 +26,29 @@ class ClamGtk():
 		self.about.hide()
 
 	def scan_file(self,widget):
-		self.choose_file = self.builder.get_object('choose_file')
-		response = self.choose_file.run()
+		choose_file = gtk.FileChooserDialog(title='Choose File for scan... '
+			,parent=None
+			,action=gtk.FILE_CHOOSER_ACTION_OPEN
+			,buttons=(gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL,gtk.STOCK_OPEN,gtk.RESPONSE_OK))
+		response = choose_file.run()
 		if response == gtk.RESPONSE_OK:
-			filename = self.choose_file.get_filename()
+			filename = choose_file.get_filename()
 			self.logbuff.insert_at_cursor('start file scan:\n')
 			log = self.clamd.scan_file(filename)
 			if log <> None:
 				for key,value in log.items():
 					self.logbuff.insert_at_cursor(key+str(value)+'\n')
 			self.logbuff.insert_at_cursor('END\n\n\n')
-		self.choose_file.hide()
+		choose_file.hide()
 
 	def scan_dir(self,widget):
-		self.choose_dir = self.builder.get_object('choose_dir')
-		response = self.choose_dir.run()
+		choose_dir = gtk.FileChooserDialog(title='Choose Dir for scan... '
+			,parent=None
+			,action=gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER
+			,buttons=(gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL,gtk.STOCK_OPEN,gtk.RESPONSE_OK))
+		response = choose_dir.run()
 		if response == gtk.RESPONSE_OK:
-			filename = self.choose_dir.get_filename()
+			filename = choose_dir.get_filename()
 			self.logbuff.insert_at_cursor('start dir scan:\n')
 			log = self.clamd.multiscan_file(filename)
 			if log <> None:
@@ -49,7 +56,7 @@ class ClamGtk():
 					print key,value
 					self.logbuff.insert_at_cursor(key+str(value)+'\n')
 			self.logbuff.insert_at_cursor('END\n\n\n')
-		self.choose_dir.hide()
+		choose_dir.hide()
 
 	def do_quit(self,widget):
 		#self.config.write(open(configfile,'w'))
